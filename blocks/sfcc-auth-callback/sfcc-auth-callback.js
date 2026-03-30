f464d5fce16e544373ae3ba971ce877f2bdd6dec
Nitesh Lakhwani <nitesh.lakhwani@ey.com>
2026-03-30 17:05:48 -0500

    fix: Remove SFCC blocks that were breaking page styling - keep only HCL integration code

--- blocks/sfcc-auth-callback/sfcc-auth-callback.js
+++
@@ -1,9 +0,0 @@
-export default async function decorate(block) {
-  const div = document.createElement('div');
-  div.innerHTML = `
-      <div>
-        <p>Authenticating...</p>
-      </div>
-    `;
-  block.appendChild(div);
-}
