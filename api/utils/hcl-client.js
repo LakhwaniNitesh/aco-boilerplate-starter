9313d8b7704489cce410d1d1c30b1f5413934ed7
Nitesh Lakhwani <nitesh.lakhwani@ey.com>
2026-04-07 23:00:54 -0500

    fix: Add langId=1 parameter back to cart endpoint (was in successful Postman request)

--- api/utils/hcl-client.js
+++ api/utils/hcl-client.js
@@ -177,7 +177,7 @@ class HCLClient {
       
       return await this.request(
         'POST',
-        `${this.baseUrl}/cart?responseFormat=json`,
+        `${this.baseUrl}/cart?langId=1&responseFormat=json`,
         requestBody,
         accessToken
       );
